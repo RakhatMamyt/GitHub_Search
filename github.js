fde06e26a4b40ed74c99b2b30c6e96724eee57bf
Rakhat Mamyt <rakhat.mamyt@gmail.com>
2022-07-20 14:44:10 -0700

    initial commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "e1699982faec128e8844";
+    this.client_secret = "6ca03b3fbee908c63ca1ff2c306f5b3b7bc4a55b";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
